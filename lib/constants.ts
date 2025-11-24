2d4a7c4967ad2de2b390ac0c4f8c9264efa19b35
Sushant Tripathu <sushant@RS-L-25-02-Macair.local>
2025-11-24 11:57:04 +0545

    treasury

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,5 @@ export const CONTRACT_ADDRESSES = {
   // Update this with your deployed contract address
   OFFICE_LOTTERY: "0xE7A361db5C95809fd06eC0a1C6c38E55E9E7b68f", // Replace with actual address
   VRF: "0x826bd98cab7150d18dfcd02a0adb3534ae1152d6", // Replace with actual address
+  TREASURY: "0x28276104599176894b7682669259512058614034", // Replace with actual address
 } as const;
